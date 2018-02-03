1dafc4a53becfda0a54e3e86a2b848e719365ebd
us͡an̸df͘rien͜ds͠ <usandfriends@gmail.com>
2018-02-02 17:02:32 -0800

    Initial commit

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,12 +1,36 @@
-var socket = io();
+// TODO:
+// [X] Escape HTML in messages
 
-socket.on('message', function(msg) {
-	$('#messages').append('<li>' + msg + '</li>')
-})
+var socket = io()
 
+// Send nickname
+socket.emit('nickname', prompt('Enter a nickname'))
+
+// Message submission handler
 $('form').submit(function() {
 	socket.emit('message', $('#m').val());
 	$('#m').val('');
 
 	return false;
 });
+
+// Events
+
+socket.on('user joined', function(id) {
+	id = escapeHtml(id)
+
+	$('#messages').append('<li><b>' + id + '</b> joined!')
+})
+
+socket.on('user disconnected', function(id) {
+	id = escapeHtml(id)
+
+	$('#messages').append('<li><b>' + id + '</b> left.')
+})
+
+socket.on('message', function(pkt) {
+	pkt.id = escapeHtml(pkt.id)
+	pkt.msg = escapeHtml(pkt.msg)
+
+	$('#messages').append('<li><b>' + pkt.id + '</b>: ' + pkt.msg + '</li>')
+})
